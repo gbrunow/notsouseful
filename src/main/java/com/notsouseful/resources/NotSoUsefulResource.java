406b8914e910768bd8637a05173d2d77100c8718
Gui Brunow <gbrunow@outlook.com>
2022-04-17 17:54:03 -0500

    cleanup + number validation

--- src/main/java/com/notsouseful/resources/NotSoUsefulResource.java
+++ src/main/java/com/notsouseful/resources/NotSoUsefulResource.java
@@ -1,31 +1,21 @@
 package com.notsouseful.resources;
 
+import com.notsouseful.core.SafeBigDecimal;
+import com.notsouseful.core.Stats;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.Valid;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response.Status;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.notsouseful.core.Stats;
-
-import io.dropwizard.validation.Validated;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.core.MediaType;
 
 @Path("nsu")
 public class NotSoUsefulResource {
     private Stats stats = new Stats();
-    
+
     @GET
     public String getNSU() {
         return "A Not So Useful Resource.";
@@ -34,7 +24,7 @@ public class NotSoUsefulResource {
     @POST
     @Path("push-and-recalculate")
     @Produces(MediaType.APPLICATION_JSON)
-    public Stats PushAndRecalculate(String number) {
+    public Stats PushAndRecalculate(@Valid @NotEmpty(message = "is missing a required parameter") String number) {
         SafeBigDecimal safe = new SafeBigDecimal(number);
 
         if (safe.getBigDecimal() == null) {
@@ -44,24 +34,5 @@ public class NotSoUsefulResource {
         this.stats.addSample(safe.getBigDecimal());
 
         return this.stats;
-    }
-}
-
-class SafeBigDecimal {
-    private final BigDecimal decimal;
-
-    public SafeBigDecimal(String number) {
-        BigDecimal temp = null;
-        try {
-            temp = new BigDecimal(number);
-        } catch (NumberFormatException ex) {
-            temp = null;
-        } finally {
-            this.decimal = temp;
-        }
-    }
-
-    public BigDecimal getBigDecimal() {
-        return this.decimal;
     }
 }
